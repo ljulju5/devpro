6564b8fb4cedd030895bc3b42c190aaa9e17991d
Ljubica Carkic <carkic5@gmail.com>
2020-11-15 11:22:36 +0100

    initial commit - nav, header and services section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+$(window).on('scroll', function() {
+    var scroll = $(window).scrollTop();
+    if(scroll >= 130) {
+        $('.sticky').addClass('stickyAdd');
+    } 
+    else {
+        $('.sticky').removeClass('stickyAdd');
+    }
+})
