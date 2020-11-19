1f2f0782a2d1648b290dfb3f576e7bb0a3becb17
Ljubica Carkic <carkic5@gmail.com>
2020-11-19 09:08:50 +0100

    aside menu in services fixed on scroll

--- js/main.js
+++ js/main.js
@@ -9,3 +9,33 @@ $(window).on('scroll', function() {
     }
 })
 
+// fix aside menu on scroll
+//store the element
+var $cache = $('.stickyAside');
+
+//store the initial position of the element
+//var vTop = $cache.offset().top - parseFloat($cache.css('margin-top').replace(/auto/, 0));
+$(window).on('scroll', function(){
+    var scroll = $(this).scrollTop();
+
+    // whether that's below the form
+    if(scroll >= 450) {
+      // if so, ad the fixed class
+      $cache.addClass('stuck');
+    }
+    else {
+      // otherwise remove it
+      $cache.removeClass('stuck');
+    }
+  });
+
+$(document).scroll(function() {
+    checkOffset();
+});
+function checkOffset() {
+    if($('.stickyAside').offset().top + $('.stickyAside').height() 
+                                           >= $('footer').offset().top - 10)
+        $('.stickyAside').css('position', 'absolute');
+    if($(document).scrollTop() + window.innerHeight < $('footer').offset().top)
+        $('.stickyAside').css('position', 'fixed'); // restore when you scroll up
+}
